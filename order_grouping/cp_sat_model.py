6679a48a2d8916ecf1dd8e16b642bb84326d5525
Ilya Egorov <egorovilyak@yandex.ru>
2025-10-01 18:09:43 +0300

    non-ready order delivery forbiden & minimize task changed

--- order_grouping/cp_sat_model.py
+++ order_grouping/cp_sat_model.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
+from collections import defaultdict
 from dataclasses import dataclass
+from datetime import datetime
 from typing import Dict, List, Tuple, TYPE_CHECKING
 
 from ortools.sat.python import cp_model
@@ -15,6 +17,13 @@ class ModelArtifacts:
     # x: cp_model.IntVar
     # y: cp_model.IntVar
     # z: cp_model.IntVar
+
+
+def _hhmmss_to_minutes(value: str) -> int:
+    timestamp = datetime.strptime(value, "%H:%M:%S")
+    total_seconds = timestamp.hour * 3600 + timestamp.minute * 60 + timestamp.second
+    return total_seconds // 60
+
 
 def build_cp_sat_model(data: "CPData") -> str: #ModelArtifacts:
     """Solve the sample CP-SAT optimization problem from Google OR-Tools docs."""
@@ -35,22 +44,30 @@ def build_cp_sat_model(data: "CPData") -> str: #ModelArtifacts:
     order_count = len(orders)
 
     if order_count == 0:
-        return ModelArtifacts(model=model, arcs={}, ranks={}, nodes=[0], order_nodes=[], couriers=[])
+        return {}
 
     nodes = list(range(order_count + 1))  # 0 represents the kitchen
     order_nodes = nodes[1:]
     couriers = list(range(len(data.couriers)))
     travel_times = data.travel_time.matrix
 
+    current_time_minutes = _hhmmss_to_minutes(data.current_time_utc)
+    ready_times = [current_time_minutes + order.prep_duration_left_minutes for order in orders]
+    horizon = 24 * 60 * 2
+
 ### минимизация длины маршрута
 
     arcs: Dict[Tuple[int, int, int], cp_model.IntVar] = {}
+    arrival_times: Dict[Tuple[int, int], cp_model.IntVar] = {}
     for courier in couriers:
         for i in nodes:
             for j in nodes:
                 if i == j:
                     continue
                 arcs[(courier, i, j)] = model.NewBoolVar(f"arc_c{courier}_{i}_{j}")
+        for node in nodes:
+            arrival_times[(courier, node)] = model.NewIntVar(0, horizon, f"arrival_c{courier}_{node}")
+        model.Add(arrival_times[(courier, 0)] == current_time_minutes)
 
     objective_terms = []
     for courier in couriers:
@@ -62,21 +79,38 @@ def build_cp_sat_model(data: "CPData") -> str: #ModelArtifacts:
                 arc_var = arcs[(courier, i, j)]
                 objective_terms.append(cost * arc_var)
 
+                model.Add(
+                    arrival_times[(courier, j)]
+                    >= arrival_times[(courier, i)] + travel_times[i][j] - horizon * (1 - arc_var)
+                )
+
     model.Minimize(sum(objective_terms))
 
 ### Ограничение, что каждый заказ должен быть посещён ровно 1 раз
 
+    incoming_by_courier: Dict[Tuple[int, int], List[cp_model.IntVar]] = defaultdict(list)
     for order_node in order_nodes:
-        incoming = []
+        incoming_total: List[cp_model.IntVar] = []
         for courier in couriers:
             for i in nodes:
                 if i == order_node:
                     continue
-                incoming.append(arcs[(courier, i, order_node)])
-        model.Add(sum(incoming) == 1)
+                arc_var = arcs[(courier, i, order_node)]
+                incoming_total.append(arc_var)
+                incoming_by_courier[(courier, order_node)].append(arc_var)
+        model.Add(sum(incoming_total) == 1)
 
-### ограничение, что заказ нельзя везти до времени
+### ограничение, что заказ нельзя везти до времени его готовности
 
+    for order_index, order_node in enumerate(order_nodes):
+        ready_time = ready_times[order_index]
+        for courier in couriers:
+            incoming_arcs = incoming_by_courier[(courier, order_node)]
+            if not incoming_arcs:
+                continue
+            visit = model.NewBoolVar(f"visit_c{courier}_{order_node}")
+            model.Add(visit == sum(incoming_arcs))
+            model.Add(arrival_times[(courier, order_node)] >= ready_time).OnlyEnforceIf(visit)
 
 ### solve
 
@@ -93,11 +127,27 @@ def build_cp_sat_model(data: "CPData") -> str: #ModelArtifacts:
     #                 continue
     #             print(f"arc_c{courier}_{i}_{j}: {solver.Value(arcs[(courier, i, j)])}")
 
+    for courier in couriers:
+        next_hop: Dict[int, int] = {}
+        for i in nodes:
+            for j in nodes:
+                if i == j:
+                    continue
+                if solver.Value(arcs[(courier, i, j)]):
+                    next_hop[i] = j
+        route = [0]
+        current = 0
+        visited = set()
+        while current in next_hop and current not in visited:
+            visited.add(current)
+            nxt = next_hop[current]
+            route.append(nxt)
+            current = nxt
+        print(f"courier {courier} route: {' -> '.join(map(str, route))}")
+
     solver_dict = MessageToDict(solver.ResponseProto())
 
-    # print(solver_dict)
     
-    # model.Maximize(x + 2 * y + 3 * z)
 
 ###
 
